fa88e5f65168a5a193a3f0f3b8120fb036169be0
François Jiang <francois.jiang@centrale-marseille.fr>
2021-07-07 18:04:07 +0200

    add price in OrderSummary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -21,6 +21,7 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientsSummary}
             </ul>
+            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
             <div className={classes.Footer}>
                 <p>Continue to Checkout?</p>
                 <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
