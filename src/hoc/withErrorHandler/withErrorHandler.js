c3e7daa14b85900552441e61bba36d64bd54499d
François Jiang <francois.jiang@centrale-marseille.fr>
2021-07-09 18:35:16 +0200

    improve error handling

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -11,13 +11,18 @@ const withErrorHandler = (WrappedComponent, axios) => {
         }
 
         componentWillMount() {
-            axios.interceptors.response.use(req => {
+            this.reqInterceptor = axios.interceptors.response.use(req => {
                 this.setState({error: null});
                 return req;
-            })
-            axios.interceptors.response.use(res => res, error => {
+            });
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error});
             })
+        }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.request.eject(this.resInterceptor);
         }
 
         errorConfirmedHandler = () => {
