8abe16103e20f01ade585dc6541cad0e5315de5e
François Jiang <francois.jiang@centrale-marseille.fr>
2021-07-10 11:28:42 +0200

    adding Orders

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
+
+
+class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res => {
+                const fletchedOrders = [];
+
+                for (let key in res.data) {
+                    fletchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+
+                this.setState({loading: false, orders: fletchedOrders});
+                console.log(fletchedOrders);
+            })
+            .catch(err => {
+                this.setState({loading: false});
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.orders.map(order => (
+                    <Order
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={order.price}
+                    />
+                ))}
+            </div>
+        );
+    }
+}
+
+export default withErrorHandler(Orders, axios);
